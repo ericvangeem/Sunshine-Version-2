0bf7453c38e4e11dc0b14548940af4239886e673
Eric VanGeem <evangeem1@gmail.com>
2016-03-22 13:06:44 -0500

    attach an item click listener to the forecastAdapter. this listener will display the clicked item's contents as a Toast message.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,7 +47,7 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        final ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
 
         mForecastAdapter = new ArrayAdapter<>(
                 getActivity(),
@@ -55,6 +57,13 @@ public class ForecastFragment extends Fragment {
         new FetchWeatherTask().execute("60606");
 
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String weatherItem = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), weatherItem, Toast.LENGTH_LONG).show();
+            }
+        });
 
         return rootView;
     }
