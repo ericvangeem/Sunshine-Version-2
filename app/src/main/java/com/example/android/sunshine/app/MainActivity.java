48349736ddcc62ead06bb6f27c5db6331fd8f3c0
Eric VanGeem <evangeem1@gmail.com>
2016-05-14 17:12:48 -0500

    add new menu item to show preferred location in a map by launching an implicit intent.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,11 +37,34 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int itemId = item.getItemId();
 
-        if (itemId == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
+        switch (itemId) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.action_view_map:
+                openPreferredLocationInMap();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        final String preferredZip = PreferenceManager.getDefaultSharedPreferences(
+                getApplicationContext()).getString(
+                getResources().getString(R.string.pref_location_key),
+                getResources().getString(R.string.pref_location_default));
+
+        final Uri geoLocation = Uri.parse("geo:0.0?").buildUpon()
+                .appendQueryParameter("q", preferredZip)
+                .build();
+
+        final Intent intent = new Intent(Intent.ACTION_VIEW, geoLocation);
+
+        // If an app exists to handle this intent, launch it.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.warning_no_map_app, Toast.LENGTH_SHORT).show();
+        }
     }
 }
